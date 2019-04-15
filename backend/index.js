dd7db26ffd0a7dd9c0b1e97383955cb6032117a5
John Costales <j.g.costales96@gmail.com>
2019-04-15 17:54:14 +0100

    Heroku fix

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const passport = require("passport");
 const path = require('path')
 
 // Models
-const profile = require("../backend/api/profile");
-const userAuth = require("../backend/api/userAuth");
-const blog = require("../backend/api/blogs");
+const profile = require("./api/profile");
+const userAuth = require("./api/userAuth");
+const blog = require("./api/blogs");
 
 const app = express();
 
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   })
 }
 
