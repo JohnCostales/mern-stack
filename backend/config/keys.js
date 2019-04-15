f625fdcee5dab7b82b6e79d25130738090ff2491
John Costales <j.g.costales96@gmail.com>
2019-04-15 18:59:37 +0100

    Heroku fix

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,7 +1,7 @@
 // Just for setting up Keys to protect the database information
 // Production & Developement keys
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys_prod');
+  module.exports = require('./keys_prod');
 } else {
-    module.exports = require('./keys_dev');
+  module.exports = require('./keys_dev');
 }
