d4562591e1403bd3e776d6289b066d8f4f78d121
John Costales <j.g.costales96@gmail.com>
2019-04-15 15:45:45 +0100

    fixing heroku

--- server/index.js
+++ server/index.js
@@ -1,31 +1,48 @@
 // Libraries
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const passport = require("passport")
 const path = require('path')
 
 // Models
-const profile = require("../backend/api/profile");
-const userAuth = require("../backend/api/userAuth");
-const blog = require("../backend/api/blogs");
+const profile = require("../backend/api/profile")
+const userAuth = require("../backend/api/userAuth")
+const blog = require("../backend/api/blogs")
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => res.send('test?'))
+// app.get('/', (req, res) => res.send('test?'))
+
+// Database Configuration
+const db = require("../config/keys").mongoURI
+
 
 // Body-Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 // Use Routes
-app.use("/api/profile", profile);
-app.use("/api/authentication", userAuth);
-app.use("/api/blogs", blog);
+app.use("/api/profile", profile)
+app.use("/api/authentication", userAuth)
+app.use("/api/blogs", blog)
 
-//Server static assets if in production
-if (process.env.NODE_ENV == 'production') {
-  //Set static folder
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport Config
+require("../config/passport")(passport)
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
@@ -33,21 +50,7 @@ if (process.env.NODE_ENV == 'production') {
   })
 }
 
-// Database Configuration
-const db = require("../config/keys").mongoURI;
-
-// Connect to MongoDB
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport Config
-require("../config/passport")(passport);
 // Run on heroku or local port 3000
-const port = process.env.port || 5000;
+const port = process.env.port || 5000
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
