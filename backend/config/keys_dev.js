668ae71de490347ac51fe77881b3722164691d2c
John Costales <j.g.costales96@gmail.com>
2019-04-15 18:32:27 +0100

    Heroku fix

--- backend/config/keys_dev.js
+++ backend/config/keys_dev.js
@@ -1,7 +1,7 @@
 // Connection port to Mongo Cloud
 module.exports = {
     mongoURI:
-        'mongodb+srv://SpiceBag:secret1234@cluster0-dg1qb.mongodb.net/test',
+        'mongodb+srv://SpiceBag:secret1234@cluster0-dg1qb.mongodb.net/test?retryWrites=true',
     secretOrKey:
         'secret'
 }
